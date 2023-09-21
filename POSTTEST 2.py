8f946e19542a62b3a7175b285dcd6be96f2b6c88
GalaxionZero <zetayu24@gmail.com>
2023-09-21 13:33:53 +0800

    Posttest 2

--- POSTTEST 2.py
+++ POSTTEST 2.py
@@ -4,9 +4,9 @@ kelas = str(input("Masukkan Kelas anda: "))
 umur = int(input("Masukkan Umur anda: "))
 tinggibadan = float(input("Masukkan Tinggi Badan anda (cm): "))
 beratbadan = float(input("Masukkan Berat Badan anda (kg): "))
-intnim = str(nim)
-terakhirnim = intnim[-3:]
-nim3terakhir = int(terakhirnim)
+strnim = str(nim)
+intnim = strnim[-3:]
+nim3terakhir = int(intnim)
 
 print(f"Nama saya {nama} dengan NIM {nim} dari kelas {kelas}. Saya berumur {umur} tahun dengan tinggi badan {tinggibadan}cm dan berat badan {beratbadan}kg")
 print(f"Tiga angka terakhir NIM anda adalah {nim3terakhir}")
