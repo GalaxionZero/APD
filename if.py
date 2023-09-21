6b89cbb42a07c771fc37a2ea9a52120413c1abf9
GalaxionZero <zetayu24@gmail.com>
2023-09-21 13:25:03 +0800

    update

--- if.py
+++ if.py
@@ -3,7 +3,7 @@ y   = int(input("Masukkan Y Disini:"))
 xy  = y * x
 if xy > 2:
     print ("xy is greater than two")
-elif xy == 2:
-    print ("xy is two")
-else:
-    print ("xy is smaller than two")
+# elif xy == 2:
+#     print ("xy is two")
+# else:
+#     print ("xy is smaller than two")
